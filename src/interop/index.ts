b357bb4f72877873e6b8c4340540e60604e6a00c
Kevin ALBRECHT <bubbuls@gmail.com>
2022-10-23 20:54:06 +0900

    fix: overlay page init overlay send an init event so b64 can be sent back commands should be async window decoration can be displayed but bg bug occuring

--- src/interop/index.ts
+++ src/interop/index.ts
@@ -4,6 +4,6 @@ export async function invokeReadFileAsb64(path: string): Promise<string> {
   return invoke("read_file", { path });
 }
 
-export async function invokeOpenOverlay(): Promise<never> {
+export async function invokeOpenOverlay(){
   return invoke("open_overlay");
 }
