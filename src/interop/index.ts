573b156a36aa56f6a81cbab968a72d962e4ec83a
Kevin ALBRECHT <bubbuls@gmail.com>
2022-10-24 02:22:42 +0900

    feat: Update current image in same window

--- src/interop/index.ts
+++ src/interop/index.ts
@@ -1,13 +1,23 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import { WebviewWindow } from "@tauri-apps/api/window";
+
+export const OVERLAY_WINDOW_ID = "overlay";
+
+export const getOverlayWindow = () =>
+  WebviewWindow.getByLabel(OVERLAY_WINDOW_ID);
 
 export async function invokeReadFileAsb64(path: string): Promise<string> {
   return invoke("read_file", { path });
 }
 
 export async function invokeOpenOverlay() {
-  return invoke("open_overlay");
+  const currentWindow = getOverlayWindow();
+
+  if (currentWindow) return true;
+  await invoke("open_overlay");
+  return false;
 }
 
 export async function invokeResizeWindow(width: number, height: number) {
-  return invoke("resize_overlay", {width,height} );
+  return invoke("resize_overlay", { width, height });
 }
